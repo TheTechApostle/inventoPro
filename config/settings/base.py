827adef6d9d464df3197bf96b831819baa9fa79d
Wonder Bassey <142162416+TheTechApostle@users.noreply.github.com>
2026-07-13 23:45:58 +0100

    Update base.py

--- config/settings/base.py
+++ config/settings/base.py
@@ -265,11 +265,23 @@ MEDIA_ROOT = BASE_DIR / "media"
 # ──────────────────────────────────────────────
 # CORS
 # ──────────────────────────────────────────────
-CORS_ALLOW_ALL_ORIGINS = config("DEBUG", cast=bool, default=False)
+# CORS_ALLOW_ALL_ORIGINS = config("DEBUG", cast=bool, default=False)
+# CORS_ALLOWED_ORIGINS = config(
+#     "CORS_ALLOWED_ORIGINS",
+#     default="http://localhost:3000,http://127.0.0.1:3000",
+# ).split(",")
+CORS_ALLOW_ALL_ORIGINS = False
+
 CORS_ALLOWED_ORIGINS = config(
     "CORS_ALLOWED_ORIGINS",
-    default="http://localhost:3000,http://127.0.0.1:3000",
+    default=""
 ).split(",")
+
+CSRF_TRUSTED_ORIGINS = config(
+    "CSRF_TRUSTED_ORIGINS",
+    default=""
+).split(",")
+
 print("ALLOWED_HOSTS =", ALLOWED_HOSTS)
 CORS_ALLOW_CREDENTIALS = True
 CORS_ALLOW_HEADERS = [
